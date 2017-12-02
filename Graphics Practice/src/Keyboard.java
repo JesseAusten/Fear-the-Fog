0c489b589a71f8c9b051adef767f7449fe53e8d5
Ben <Ben@192.168.3.104>
2017-12-02 12:28:57 -0500

    Keyboard controlled animation updated

--- Graphics Practice/src/Keyboard.java
+++ Graphics Practice/src/Keyboard.java
@@ -1,6 +1,10 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Window;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.ImageIcon;
 
 public class Keyboard extends Core implements KeyListener {
 
@@ -11,6 +15,9 @@ public class Keyboard extends Core implements KeyListener {
 	}
 
 	private String mess = "";
+	private Image bg;
+	private Animation a;
+	private Sprites sprite;
 
 	// init also init from superclass
 
@@ -21,6 +28,14 @@ public class Keyboard extends Core implements KeyListener {
 		w.setFocusTraversalKeysEnabled(false);
 		w.addKeyListener(this);
 		mess = "press escape to exit";
+
+		bg = new ImageIcon("E:\\Pictures\\batman.jpg").getImage().getScaledInstance(3840, 2160, 0);
+		Image banner1 = new ImageIcon("E:\\Pictures\\logo.png").getImage();
+		a = new Animation();
+
+		a.addScene(banner1, 250);
+		a.addScene(banner1, 250);
+		sprite = new Sprites(a);
 
 	}
 
@@ -38,6 +53,29 @@ public class Keyboard extends Core implements KeyListener {
 			e.consume();
 		}
 
+		switch (keyCode) {
+
+		case KeyEvent.VK_S:
+			sprite.setVelocityY(0.5f);
+			e.consume();
+			break;
+		case KeyEvent.VK_W:
+			sprite.setVelocityY(-0.5f);
+			e.consume();
+			break;
+
+		case KeyEvent.VK_D:
+			sprite.setVelocityX(0.5f);
+			e.consume();
+			break;
+
+		case KeyEvent.VK_A:
+			sprite.setVelocityX(-0.5f);
+			e.consume();
+			break;
+
+		}
+
 	}
 
 	// key released
@@ -47,29 +85,81 @@ public class Keyboard extends Core implements KeyListener {
 		int keyCode = e.getKeyCode();
 		mess = "Released: " + KeyEvent.getKeyText(keyCode);
 		e.consume();
+
+		switch (keyCode) {
+
+		case KeyEvent.VK_S:
+			sprite.setVelocityY(0);
+			e.consume();
+			break;
+		case KeyEvent.VK_W:
+			sprite.setVelocityY(0);
+			e.consume();
+			break;
+
+		case KeyEvent.VK_D:
+			sprite.setVelocityX(0);
+			e.consume();
+			break;
+
+		case KeyEvent.VK_A:
+			sprite.setVelocityX(0);
+			e.consume();
+			break;
+
+		}
 	}
 
-	// last method from iterface
+	// last method from interface
 
 	public void keyTyped(KeyEvent e) {
 
 		e.consume();
 	}
-	
-	//draw
-	
+
+	// draw
+
 	public synchronized void draw(Graphics2D g) {
-		
+
 		Window w = s.getFullScreenWindow();
-		
+
 		g.setColor(w.getBackground());
 		g.fillRect(0, 0, s.getWidth(), s.getHeight());
-		g.setColor(w.getForeground());
 		g.drawString(mess, 1000, 1000);
+		g.drawImage(bg, 0, 0, null);
+		
+		g.drawImage(sprite.getImage(), Math.round(sprite.getX()), Math.round(sprite.getY()), null);
+	}
+
+	public void update(long timePassed) {
+
+		 if (sprite.getX() < 0) {
+		
+		 sprite.setVelocityX(0);
+		 sprite.setX(1);
+		 
+		 }else if(sprite.getX() + sprite.getWidth() >= s.getWidth()) {
+		
+		 sprite.setVelocityX(0);
+		 sprite.setX((s.getWidth() - 1) - sprite.getWidth());
+		 
+		
+		 }
+		
+		 if (sprite.getY() < 0) {
+		
+		 sprite.setVelocityY(0);
+		 sprite.setY(1);
+		
+		 }else if(sprite.getY()+ sprite.getHeight() >= s.getHeight()) {
+		
+		 sprite.setVelocityY(0);
+		 sprite.setY((s.getHeight() - 1) - sprite.getHeight());
+		
+		 }
+
+		sprite.update(timePassed);
+
 	}
 
 }
-
-
-
-
