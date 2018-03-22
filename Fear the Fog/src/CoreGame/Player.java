9e6226e9239cf23e7fc81d36bcc3ac616f2bc800
Ben <bendscanlon@hotmail.com>
2018-03-22 00:31:06 -0400

    Early push, planning stage 22-03-2018

--- Fear the Fog/src/CoreGame/Player.java
+++ Fear the Fog/src/CoreGame/Player.java
@@ -2,4 +2,28 @@ package CoreGame;
 
 public class Player {
 
+	private int column;
+	private int row;
+	private Tile[] tileSet;
+	private boolean isAlive;
+	private int[] columnShape;
+	private int[] rowShape;
+
+	public Player(int column, int row) {
+
+		this.column = column;
+		this.row = row;
+		this.isAlive = true;
+
+		tileSet = new Tile[columnShape.length];
+		tileSet[0] = new Tile(true, true, true, column, row);
+
+		for (int i = 0; i < (tileSet.length - 1); i++) {
+
+			tileSet[i + 1] = new Tile(true, true, false, columnShape[i], rowShape[i]);
+
+		}
+
+	}
+
 }
--- Fear the Fog/src/CoreGame/Tile.java
+++ Fear the Fog/src/CoreGame/Tile.java
@@ -1,14 +1,68 @@
 package CoreGame;
 
 public class Tile {
-	
-	boolean passable;
-	boolean destructable;
-	
-	public Tile() {
-		
-		System.out.println();
+
+	private boolean passable;
+	private boolean destructable;
+	private boolean isAlive;
+	private int column;
+	private int row;
+
+	public Tile(boolean passable, boolean destructable, boolean isAlive, int column, int row) {
+
+		this.passable = passable;
+		this.destructable = destructable;
+		this.isAlive = isAlive;
+		this.column = column;
+		this.row = row;
+
+	}
+
+	public boolean getPassable() {
+
+		return passable;
+
+	}
+
+	public boolean getDestructable() {
+
+		return destructable;
+
+	}
+
+	public boolean getIsAlive() {
+
+		return isAlive;
+
+	}
+
+	public void setPassable(boolean passable) {
+
+		this.passable = passable;
+
+	}
+
+	public void setDestructable(boolean destructable) {
+
+		this.destructable = destructable;
+
 	}
 	
+	public void setIsAlive(boolean isAlive) {
+		
+		this.isAlive = isAlive;
 	
+	}
+	
+	public void setColumn(int column) {
+		
+		this.column = column;
+		
+	}
+	
+	public void setRow(int row) {
+		
+		this.row = row;
+	}
+
 }
